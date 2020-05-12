f62dc6736d2f5ed2366575c608ce613588bd0206
EJ Fox <ejfox@ejfox.com>
2020-05-12 15:13:43 -0400

    feat: add services page

---
+++ pages/services.vue
@@ -0,0 +1,30 @@
+<template>
+<section>
+  <h1>Services</h1>
+</section>
+</template>
+
+<script>
+// import * as d3 from 'd3'
+
+export default {
+  name: 'Services',
+  props: {},
+  data: function () {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted: function () {    
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
